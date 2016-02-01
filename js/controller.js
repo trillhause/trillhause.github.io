eea3273bccdb26f943171aad163a4951d605a9fa
Millin Gabani <gabanimillin@gmail.com>
2016-01-31 22:09:01 -0500

    added home page subtext animation

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,46 @@
 // create the controller and inject Angular's $scope
 angular.module('Portfolio').controller('MainController', function ($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
+  // create a message to display in our view
+  $scope.message = 'Everyone come and see how good I look!';
+  $scope.texttyping = ["logically minded and creative at heart^5000", "design oriented developer^5000", "street smart engineer^5000"]
+}).directive('typedjs', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      strings: '='
+    },
+    template: '<span id="typed-output"></span>',
+    link: function ($scope, $element, $attrs) {
+      var options = {
+        strings: $scope.strings,
+        typeSpeed: 40,
+        loop: true,
+        contentType: "html",
+        showCursor: true,
+        loopCount: false,
+        cursorChar: "_"
+      };
+
+      $(function () {
+        $("#typed-output").typed(options);
+      });
+    }
+  };
 });
 
+
 angular.module('Portfolio').controller('WorkController', function ($scope) {
-    $scope.message = 'Showing off my work mofocas';
+  $scope.message = 'Showing off my work mofocas';
 });
 
 angular.module('Portfolio').controller('BioController', function ($scope) {
-    $scope.message = 'Watch me Whip!! Watch me Nae Nae!!';
+  $scope.message = 'Watch me Whip!! Watch me Nae Nae!!';
 });
 
 angular.module('Portfolio').controller('UpdatesController', function ($scope) {
-    $scope.message = 'Find out more about me';
+  $scope.message = 'Find out more about me';
 });
 
 angular.module('Portfolio').controller('ContactController', function ($scope) {
-    $scope.message = 'Contact me';
+  $scope.message = 'Contact me';
 });
