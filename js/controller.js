fa132ba1de0bb337574b23da3ceac15d525d7c30
Millin Gabani <gabanimillin@gmail.com>
2016-02-10 01:51:27 -0500

    Fixed scrolling and other bugs. Partly Added Google Maps. Completed contact form

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,7 @@
 angular.module('Portfolio').controller('HomeController', function () {
   // create a message to display in our view
   this.message = 'Everyone come and see how good I look!';
+
   this.texttyping = ["logically minded and creative at heart^5000", "design oriented developer^5000", "street smart engineer^5000"]
 }).directive('typedjs', function () {
   return {
@@ -20,12 +21,12 @@ angular.module('Portfolio').controller('HomeController', function () {
         loopCount: false,
         cursorChar: "_"
       };
-
       $(function () {
         $("#typed-output").typed(options);
       });
     }
   };
+
 });
 
 angular.module('Portfolio').controller('ThankController', function () {
@@ -299,20 +300,33 @@ angular.module('Portfolio').controller('UpdatesController', ['$sce', function ($
 }]);
 
 angular.module('Portfolio').controller('ContactController', ['$http', function ($http) {
-  var contact = this;
-  this.success = false;
-  this.error = false;
-  this.visitor = {};
-  this.sendMessage = function () {
-    $http({
-      method: 'POST',
-      url: '/php/processContactForm.php',
-      data: contact.visitor,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    }).success(function (data) {
-      contact.success = true;
-    });
+  this.texttyping = ["To submit the form, please fill all the fields correctly"]
+    }]).directive('type', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      strings: '='
+    },
+    template: '<span id="typed-output"></span>',
+    link: function ($scope, $element, $attrs) {
+      var options = {
+        strings: $scope.strings,
+        typeSpeed: 40,
+        loop: false,
+        contentType: "html",
+        showCursor: true,
+        loopCount: false,
+        cursorChar: "_"
+      };
+      $(function () {
+        $("#typed-output").typed(options);
+      });
+    }
   };
-}]);
+});
+
+
+
+
+
+//
