f01d81676b2b0992fedcdcc6bb5d89bef70764d8
Millin Gabani <gabanimillin@gmail.com>
2016-02-15 20:34:17 -0500

    Blog links fixed and github links added for projects

--- js/controller.js
+++ js/controller.js
@@ -77,7 +77,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
   this.projects = [
     {
       title: "Bricks and Bytes",
-      content: $sce.trustAsHtml("<em>Bricks & Bytes was awarded second place at PCH Hackathon</em> <br><br>Bricks & Bytes is an innovative hardware platform that allows children to learn logic of programming, without the language. <br><br>Bricks & Bytes was developed at PCH Hackathon in a collaboration with seven other mechatronics student. Project includes 7 interchangeable modules that stimulates computer science concepts with real I/O feedback. <br><br>Each module called brick contains an Arduino micro-controller board to manage peripherals and UART communication, with a  designated \"transmitter\" module relaying information to an Android/iOS device via Bluetooth 2.0. (Communication between bricks are established using UART with the last brick transmitting any results to an Android/iOS application via Bluetooth.) <br><br>The purpose of the project was to make programming feel more intuitive and organic, allowing young users to familiarize themselves with the fundamental principles of the software development such as control flow and debugging."),
+      content: $sce.trustAsHtml("<em>Bricks & Bytes was awarded second place at PCH Hackathon</em> <br><br>Bricks & Bytes is an innovative hardware platform that allows children to learn logic of programming, without the language. Bricks & Bytes was developed at PCH Hackathon in a collaboration with seven other mechatronics student. Project includes 7 interchangeable modules that stimulates computer science concepts with real I/O feedback. <br><br>Each module called brick contains an Arduino micro-controller board to manage peripherals and UART communication, with a  designated \"transmitter\" module relaying information to an Android/iOS device via Bluetooth 2.0. (Communication between bricks are established using UART with the last brick transmitting any results to an Android/iOS application via Bluetooth.) <br><br>The purpose of the project was to make programming feel more intuitive and organic, allowing young users to familiarize themselves with the fundamental principles of the software development such as control flow and debugging.<br><br> <a href='https://twitter.com/search?q=bricks%20%26%20bytes%20from%3APCHHackathon&src=typd&lang=en' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -132,7 +132,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
     },
     {
       title: "uClicker",
-      content: $sce.trustAsHtml("uClicker is a portable hardware platform that allows students to answer iClicker questions from their phones. <br><br> uClicker was developed at Hack The North 2015 in collaboration with two other engineering students. It uses an Arduino micro-controller board, an RF Link transmitter and an RF Link receiver. <br><br>When the user selects an option (options can be selected using an Android app), signals are sent to the iClicker base station using the RF Link transmitter. The RF Link receiver then reads data from the base station and other iClickers which adds more features like finding  the most selected answer, mass changing/randomizing other student's answers, jamming signals during a question, etc. Using our open source platform, users are able to hack and customize their uClicker with desired functionalities. <br><br>The purpose of this project was to find an affordable alternative to iClicker and now it is possible with uClicker."),
+      content: $sce.trustAsHtml("uClicker is a portable hardware platform that allows students to answer iClicker questions from their phones. <br><br> uClicker was developed at Hack The North 2015 in collaboration with two other engineering students. It uses an Arduino micro-controller board, an RF Link transmitter and an RF Link receiver. <br><br>When the user selects an option (options can be selected using an Android app), signals are sent to the iClicker base station using the RF Link transmitter. The RF Link receiver then reads data from the base station and other iClickers which adds more features like finding  the most selected answer, mass changing/randomizing other student's answers, jamming signals during a question, etc. Using our open source platform, users are able to hack and customize their uClicker with desired functionalities. <br><br>The purpose of this project was to find an affordable alternative to iClicker and now it is possible with uClicker. <br><br> <a href='https://github.com/millingab/uClicker' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -175,7 +175,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
     },
     {
       title: "Myosic",
-      content: $sce.trustAsHtml("<em>Myosic won 'The Best Hack' award at Boilermake hackathon </em><br><br>Simple, elegant and easy to use. This app is a product of hardwork and a strong vision to improve hands free music experience. The visual hierarchy of user interface enriches the app's overall experience by creating a sense of depth. With Myosic, experience music like never before."),
+      content: $sce.trustAsHtml("<em>Myosic won 'The Best Hack' award at Boilermake hackathon </em><br><br>Simple, elegant and easy to use. This app is a product of hardwork and a strong vision to improve hands free music experience. The visual hierarchy of user interface enriches the app's overall experience by creating a sense of depth. With Myosic, experience music like never before. <br><br> <a href='http://devpost.com/software/myosic-m13n9' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -187,7 +187,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
     },
     {
       title: "Wanderlust",
-      content: $sce.trustAsHtml("<em>Wanderlust received Windows Sponsorship award at YHack. </em><br><br>This multiplayer - motion control - first person shooting game was a inspired by interest in motion control and passion for technology. Utilizing Kinect's powerful skeletal tracking system, we were able to provide game controls purely using body motion. <br><br>Environment for game is created using  Unity engine and scripting for environment and players is done in C# using Unity Environment. Game servers were hosted on Windows Azure."),
+      content: $sce.trustAsHtml("<em>Wanderlust received Windows Sponsorship award at YHack. </em><br><br>This multiplayer - motion control - first person shooting game was a inspired by interest in motion control and passion for technology. Utilizing Kinect's powerful skeletal tracking system, we were able to provide game controls purely using body motion. <br><br>Environment for game is created using  Unity engine and scripting for environment and players is done in C# using Unity Environment. Game servers were hosted on Windows Azure.<br><br> <a href='http://devpost.com/software/wanderlust-rqr0j' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -199,7 +199,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
     },
     {
       title: "Tron 2019",
-      content: $sce.trustAsHtml("<em>Around 300 sweaters with Tron 2019 design bought by Mechatronics Engineering class of 2019 at University of Waterloo. Asked multiple times by other tron classes to design one for their year.</em><br><br> Tron 2019 was a design project undertaken during summer 2015 to create a brand for Mechatronics Engineering class of 2019. This design reflects the core concepts of Mechatronics Engineering while keeping the modern flat design in mind. Everything from selecting color schemes to finishing touches was done with excessive attention of detail.<br><br> Color scheme was this design was a challenges, as I wanted tron 2019 brand to go seamlessly with most popular sweater colors. After a long inspection, I was able to design a perfect color scheme that worked with all the famous sweater colors. <br><br>Tron 2019 was a great success and is greatly appreciated by colleagues and friends to this date. Fellow trons are found wearing tron 2019 sweater/hoodie with pride. I am grateful to be a part of such a amazing movement and happy to bring smiles to hundreds of faces."),
+      content: $sce.trustAsHtml("<em>Around 300 sweaters with Tron 2019 design bought by Mechatronics Engineering class of 2019 at University of Waterloo. Asked multiple times by other tron classes to design one for their year.</em><br><br> Tron 2019 was a design project undertaken during summer 2015 to create a brand for Mechatronics Engineering class of 2019. This design reflects the core concepts of Mechatronics Engineering while keeping the modern flat design in mind. Everything from selecting color schemes to finishing touches was done with excessive attention of detail.<br><br> Color scheme was this design was a challenges, as I wanted tron 2019 brand to go seamlessly with most popular sweater colors. After a long inspection, I was able to design a perfect color scheme that worked with all the famous sweater colors. <br><br>Tron 2019 was a great success and is greatly appreciated by colleagues and friends to this date. Fellow trons are found wearing tron 2019 sweater/hoodie with pride. I am grateful to be a part of such a amazing movement and happy to bring smiles to hundreds of faces.<br><br> <a href='https://www.instagram.com/explore/tags/tron2019/' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -242,7 +242,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
     },
     {
       title: "Desk",
-      content: $sce.trustAsHtml("Desk is a photography project conducted during moving houses. It does a excellent job of defining me by demonstration of my quotidian tools. These tools are a extended part of me and are of extreme importance."),
+      content: $sce.trustAsHtml("Desk is a photography project conducted during moving houses. It does a excellent job of defining me by demonstration of my quotidian tools. These tools are a extended part of me and are of extreme importance.<br><br> <a href='https://www.instagram.com/p/zbVaPJhTIz/' target=\"_blank\">&lang;Learn More&rang;</a>"),
       current: 0,
       images: [
         {
@@ -270,7 +270,7 @@ angular.module('Portfolio').controller('WorkController', ['$sce', '$http', funct
 
 angular.module('Portfolio').controller('BioController', function () {});
 
-angular.module('Portfolio').controller('UpdatesController', ['$sce', '$location', '$anchorScroll', function ($sce, $location, $anchorScroll) {
+angular.module('Portfolio').controller('BlogController', ['$sce', '$location', '$anchorScroll', function ($sce, $location, $anchorScroll) {
   this.current = null;
   this.setArticle = function (value) {
     this.articles[value].isActive = !this.articles[value].isActive;
@@ -283,28 +283,22 @@ angular.module('Portfolio').controller('UpdatesController', ['$sce', '$location'
 
   this.articles = [
     {
-      id: 0,
-      isActive: false,
+      siteID: "painter-to-engineer",
       title: "Painter to Engineer",
-      content: $sce.trustAsHtml("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor ullamcorper pulvinar. Vivamus diam erat, semper in malesuada ut, dictum a mi. Integer at felis dictum, sollicitudin libero eget, posuere erat. Suspendisse ullamcorper massa eu viverra porta. Suspendisse interdum quam dolor, eget fringilla justo pharetra sed. Aliquam in dictum lectus. Nulla consequat lacus at pretium auctor. Praesent maximus diam pharetra elit interdum imperdiet. Etiam rutrum venenatis lacus, at luctus diam commodo eget. Aliquam congue, lectus vitae malesuada scelerisque, sem nulla suscipit urna, a pretium quam arcu ac augue. Sed interdum consequat suscipit. Mauris eu orci a dolor viverra finibus. Mauris blandit nulla rutrum pretium ornare. Sed quis enim lectus.<br><br> Nullam eget pharetra orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras at libero id elit auctor volutpat. In sit amet purus at libero viverra pretium. Cras sollicitudin tempus porttitor. Aenean aliquam dolor at velit imperdiet auctor. Donec maximus pellentesque fermentum. Proin et dapibus neque. Quisque id orci quis dolor volutpat elementum."),
     },
     {
-      id: 1,
-      isActive: false,
+      siteID: "life-is-short",
       title: "Life is Short",
-      content: $sce.trustAsHtml("ulla consequat lacus at pretium auctor. Praesent maximus diam pharetra elit interdum imperdiet. Etiam rutrum venenatis lacus, at luctus diam commodo eget. Aliquam congue, lectus vitae malesuada scelerisque, sem nulla suscipit urna, a pretium quam arcu ac augue. Sed interdum consequat suscipit. Mauris eu orci a dolor viverra finibus. Mauris blandit nulla rutrum pretium ornare. Sed quis enim lectus.<br><br> Nullam eget pharetra orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras at libero id elit auctor volutpat. In sit amet purus at libero viverra pretium. Cras sollicitudin tempus porttitor. Aenean aliquam dolor at velit imperdiet auctor. Donec maximus pellentesque fermentum. Proin et dapibus neque. Quisque id orci quis dolor volutpat elementum."),
     },
     {
-      id: 2,
-      isActive: false,
+      siteID: "turning-twenty-one",
       title: "Turning Twenty-One",
-      content: $sce.trustAsHtml("ngilla justo pharetra sed. Aliquam in dictum lectus. Nulla consequat lacus at pretium auctor. Praesent maximus diam pharetra elit interdum imperdiet. Etiam rutrum venenatis lacus, at luctus diam commodo eget. Aliquam congue, lectus vitae malesuada scelerisque, sem nulla suscipit urna, a pretium quam arcu ac augue. Sed interdum consequat suscipit. Mauris eu orci a dolor viverra finibus. Mauris blandit nulla rutrum pretium ornare. Sed quis enim lectus.<br><br> Nullam eget pharetra orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras at libero id elit auctor volutpat. In sit amet<br><br> pulla consequat lacus at pretium auctor. Praesent maximus diam pharetra elit interdum imperdiet. Etiam rutrum venenatis lacus, at luctus diam commodo eget. Aliquam congue, lectus vitae malesuada scelerisque, sem nulla suscipit urna, a pretium quam arcu ac augue. Sed interdum consequat suscipit. Mauris eu orci a dolor viverra finibus. Mauris blandit nulla rutrum pretium ornare. Sed quis enim lectus.<br><br> Nullam eget pharetra orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras at libero id elit auctor volutpat. In sit amet purus at libero viverra pretium. Cras sollicitudin tempus porttitor. Aenean aliquam dolor at velit imperdiet auctor. Donec maximus pellentesque fermentum. Proin et dapibus neque. Quisque id orci quis dolor volutpat elementum."),
     },
   ];
 }]);
 
 angular.module('Portfolio').controller('ContactController', ['$http', function ($http) {
-  this.texttyping = ["To submit the form, please fill all fields correctly"]
+  this.texttyping = ["Please fill all fields to submit the form"]
     }]).directive('type', function () {
   return {
     restrict: 'E',
